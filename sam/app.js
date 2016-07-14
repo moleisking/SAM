7442178d66a840c52853e26333d4c1ef38b02032
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-07-14 13:57:27 +0200

    Added crossed domain variables

--- sam/app.js
+++ sam/app.js
@@ -12,6 +12,13 @@ var apiRoutes = require('./routes/auth');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 // // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
@@ -25,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
+app.use(allowCrossDomain);
 
 app.use('/', home);
 app.use('/users', users);
