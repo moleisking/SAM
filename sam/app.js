be696b99615a7b9587c962611f00108e99995d40
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-07-17 21:37:24 +0200

    Adding authorization as an allowed header

--- sam/app.js
+++ sam/app.js
@@ -15,7 +15,7 @@ var app = express();
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, authorization');
   next();
 };
 
