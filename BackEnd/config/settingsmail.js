d2dc9289b29edb6bd161b2fd84cb5dda204d9ffc
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-07-26 18:44:22 +0200

    Added settings email server config default.

---
+++ BackEnd/config/settingsmail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'mail': 'alvarotorpedo@gmail.com',
+    'password': '',
+    'host': 'smtp.gmail.com',
+    'port': 587,
+    'secure': true,
+    'from': 'SAM <alvarotorpedo@gmail.com>'
+};
