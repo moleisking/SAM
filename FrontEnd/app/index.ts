3cc0117138a3c4149fa1f1379e3052a356f160a0
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-08-01 18:58:11 +0300

    Delete index file as we wont use it

--- FrontEnd/app/index.ts
+++
@@ -1,6 +0,0 @@
-// export * from "./environment";
-// export * from "./app.component";
-// export * from "./routes/app.routes";
-// export * from "./services/auth";
-// export * from "./routes/route.manager";
-// export * from "./config/settings";
