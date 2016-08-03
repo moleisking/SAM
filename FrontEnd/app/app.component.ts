a1b3f0cba6b5638803bd33d616c0164e05ad4fb4
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-08-03 21:50:57 +0300

    Make visible and invisible the navbar links depending on logged or not

--- FrontEnd/app/app.component.ts
+++ FrontEnd/app/app.component.ts
@@ -11,12 +11,28 @@ import { TermsConditions } from "./components/termsconditions";
 import { ContactUs } from "./components/contactus";
 import { Categories } from "./components/categories";
 
+import { AuthService } from "./services/auth";
+
 @Component({
   selector: "app-root",
   templateUrl: "/views/app-root.html",
   styleUrls: ["/styles/app-root.css"],
   directives: [ROUTER_DIRECTIVES],
-  precompile: [Home, Register, Login, About, Dashboard, ForgottenPassword, TermsConditions, ContactUs, Categories ]
+  precompile: [Home, Register, Login, About, Dashboard, ForgottenPassword, TermsConditions, ContactUs, Categories],
+  styles: [`
+      a.disabled, a.disabled:visited {
+        pointer-events: none;
+        cursor: none;
+        color: gray;
+      }`
+  ]
 })
 
-export class AppComponent { }
+export class AppComponent {
+
+  constructor(private authService: AuthService) { }
+
+  isDisabledIfLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+}
