94f7fc8029662300d78143962c62a8b6d9a4e32f
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-09-01 20:53:55 +0300

    Added several url for CORS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,9 +14,14 @@ var work = require('./routes/work');
 var app = express();
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  var allowedOrigins = ['http://localhost:3000', 'http://localhost:63592', 'http://minitrabajo.me:3000']; 
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1)
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, authorization');
+  // res.header('Access-Control-Allow-Credentials', true);
   next();
 };
 
