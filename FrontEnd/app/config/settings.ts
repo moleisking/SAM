e2c365a081f1762fd087f592626b4821d826608e
moleisking <moleisking@gmail.com>
2016-09-02 12:41:06 +0200

    Added local servers to frontend

--- FrontEnd/app/config/settings.ts
+++ FrontEnd/app/config/settings.ts
@@ -1,3 +1,5 @@
 export const Settings = {
-    backend_url: "http://localhost:3003"
+   // backend_url: "http://localhost:3003"
+    backend_url: "http://192.168.1.100:3003"
+  //  backend_url: "http://192.168.1.110:3003"
 };
