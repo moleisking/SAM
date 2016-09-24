6efad89c89f0289414a6931fb8d4f439576b5051
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-09-24 17:05:42 +0300

    Added sendgrind to the project

--- BackEnd/core/emailer.js
+++ BackEnd/core/emailer.js
@@ -1,6 +1,6 @@
-var config = require('../config/settingsmail');
-var mailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+var config = require("../config/settingsmail");
+var mailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
 var transporter = mailer.createTransport(smtpTransport({
     service: config.service,
     auth: {
@@ -17,23 +17,47 @@ var transporter = mailer.createTransport(smtpTransport({
 //         pass: config.password
 //     }
 // });
+var settings = require("../config/settingsmail");
 
 module.exports = {
+
+    email: function (from, to, text, cb) {
+        var helper = require("sendgrid").mail;
+        var from_email = new helper.Email(from);
+        var to_email = new helper.Email(to);
+        var subject = "Hello World from the SendGrid Node.js Library!";
+        var content = new helper.Content("text/plain", text);
+        var mail = new helper.Mail(from_email, subject, to_email, content);
+
+        var sg = require("sendgrid")(settings.sendgrid);
+        var request = sg.emptyRequest({
+            method: "POST",
+            path: "/v3/mail/send",
+            body: mail.toJSON(),
+        });
+
+        sg.API(request, function (error, response) {
+            console.log(response.statusCode);
+            console.log(response.body);
+            console.log(response.headers);
+            return cb(null, response.statusCode, response.body, response.headers);
+        });
+    },
 
     forgottenpassword: function (user, cb) {
         var mailOptions = {
             from: config.from, // sender address
             to: user.email, // list of receivers
-            subject: 'SAM forgotten password', // Subject line
+            subject: "SAM forgotten password", // Subject line
             // text: text //, // plaintext body
-            html: '<b>forgotten password ✔</b>' // You can choose to send an HTML body instead
+            html: "<b>forgotten password ✔</b>" // You can choose to send an HTML body instead
         };
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
                 return cb(error, null);
             } else {
-                console.log('Message sent: ' + info.response);
+                console.log("Message sent: " + info.response);
                 return cb(null, info.response);
             };
         });
@@ -43,7 +67,7 @@ module.exports = {
         var mailOptions = {
             from: form.email, // sender address
             to: config.admin, 
-            subject: 'SAM new Contact Us Form', // Subject line
+            subject: "SAM new Contact Us Form", // Subject line
             text: form.message //, // plaintext body
         };
         transporter.sendMail(mailOptions, function (error, info) {
@@ -51,7 +75,7 @@ module.exports = {
                 console.log(error);
                 return cb(error, null);
             } else {
-                console.log('Message sent: ' + info.response);
+                console.log("Message sent: " + info.response);
                 return cb(null, info.response);
             };
         });
--- BackEnd/routes/message.js
+++ BackEnd/routes/message.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var message = require("../core/message");
+var emailer = require("../core/emailer");
 var user = require("../core/user");
 
 router.get("/readalllasts", passport.authenticate("jwt", { session: false }), function (req, res, next) {
@@ -22,11 +23,21 @@ router.get("/read/:nameUrl", passport.authenticate("jwt", { session: false }), f
 
 router.post("/add", passport.authenticate("jwt", { session: false }), function (req, res, next) {
   if (!req.body.to || !req.body.text)
-    return res.status(400).send("Please pass from, to and text.");
+    return res.status(400).send("Please pass to and text.");
   message.create(user.getEmailFromTokenUser(req.headers), req.body, function (err, data) {
     if (err)
       return res.status(500).json({ err });
     res.json({ data });
+  });
+});
+
+router.post("/email", function (req, res, next) {
+  if (!req.body.from || !req.body.to || !req.body.text)
+    return res.status(400).send("Please pass from, to and text.");
+  emailer.email(req.body.from, req.body.to, req.body.text, function (err, status, body, headers) {
+    if (err)
+      return res.status(500).json({ err });
+    res.json({ status, body, headers });
   });
 });
 
