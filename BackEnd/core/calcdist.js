cf0ce7352c2e5006fb82f2c5b7ac63e394911b99
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-09-27 16:01:40 +0300

    Change the search to just search by current coordenates

--- BackEnd/core/calcdist.js
+++ BackEnd/core/calcdist.js
@@ -3,9 +3,9 @@ module.exports = {
     CalcDist: function (coords1, coords2) {
         var m = module.exports;
         var R = 6371; // km
-        var dLat = m.toRad(parseFloat(coords2.lat) - parseFloat(coords1.lat));
-        var dLon = m.toRad(parseFloat(coords2.lng) - parseFloat(coords1.lng));
-        var lat1 = m.toRad(parseFloat(coords1.lat));
+        var dLat = m.toRad(parseFloat(coords2.lat) - parseFloat(coords1.currentLat));
+        var dLon = m.toRad(parseFloat(coords2.lng) - parseFloat(coords1.currentLng));
+        var lat1 = m.toRad(parseFloat(coords1.currentLat));
         var lat2 = m.toRad(parseFloat(coords2.lat));
 
         var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
