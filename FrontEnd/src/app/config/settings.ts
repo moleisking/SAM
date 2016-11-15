c158cf301adba4071a1633d587926460b5cb4483
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-11-15 12:09:46 +0200

    Added to ignore folders no need on the repository. Also deleted consecuent files.

--- FrontEnd/src/app/config/settings.ts
+++ FrontEnd/src/app/config/settings.ts
@@ -1,6 +1,6 @@
 export const Settings = {
-  backend_url: "http://localhost:3003",
+  backend_url: "http://minitrabajo.me:3003",
   search_country: "es",
   localization: "es",
-  prod: false
+  prod: true
 };
