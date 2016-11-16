cc66088f6a1862b0a77ab28dfb26cae32c988890
Alvaro Torpedo <goodbytes23@yahoo.es>
2016-11-16 19:48:17 +0200

    Minor format files

--- BackEnd/config/passport.js
+++ BackEnd/config/passport.js
@@ -1,20 +1,20 @@
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-var user = require('../core/user');
-var config = require('./settings');
+var JwtStrategy = require("passport-jwt").Strategy,
+    ExtractJwt = require("passport-jwt").ExtractJwt;
+var user = require("../core/user");
+var config = require("./settings");
 
 module.exports = function (passport) {
-  var opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-  opts.secretOrKey = config.secret;
-  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    user.read(jwt_payload.email, function(err, data) {
-          if (err)
-              return done(err, false);
-          if (data)
-              done(null, data);
-          else
-              done(null, false);
-      });
-  }));
+    var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+        user.read(jwt_payload.email, function (err, data) {
+            if (err)
+                return done(err, false);
+            if (data)
+                done(null, data);
+            else
+                done(null, false);
+        });
+    }));
 };
